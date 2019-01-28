11fa584fe8429db10eb4facea4fc14a1da6bb2b1
Никита Филатов <admiral2799@gmail.com>
2019-01-28 19:15:47 +0200

    Add project files.

---
+++ StockBusinessLogic/Services/ArrivalService.cs
@@ -0,0 +1,30 @@
+﻿using StockModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockBusinessLogic
+{
+    public interface IArrivalService
+    {
+        List<Arrival> GetArrivals();
+    }
+
+    public class ArrivalService: IArrivalService
+    {       
+        public List<Arrival> GetArrivals()
+        {
+            var arrivals = new List<Arrival>
+            {
+                {
+                }
+
+
+                   
+                
+            };
+        }
+    }
+}
---
+++ StockModels/DataModels/Base.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockModels.DataModels
+{
+    public abstract class Base
+    {
+        public int GoodId { get; set; }
+
+        public int Amount  { get; set; }
+
+        public DateTime Date=DateTime.Now;
+    }
+}
